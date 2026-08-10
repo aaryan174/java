32a866068f1e36ac27748c38648bddfe3d9a7413
aaryan174 <aryan007.lko@gmail.com>
2026-08-10 22:00:18 +0530

    Add basic Java program for DSA

---
+++ index.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class index {
+    public static void main(String args[]) {
+        // Scanner sc = new Scanner(System.in);
+        // int A = sc.nextInt();
+        // int B = sc.nextInt();
+        // int C = sc.nextInt();
+        // if (A>B && A>C) {
+        //     System.out.println("A bada hai");
+        // }
+        // else if (B>A && B>C) {
+        //     System.out.println("B bada hai");
+        // }
+
+        // else{
+        //     System.out.println("C Bada");
+        // }
+        int num = 0;
+
+        while (num < 100) {
+            System.out.println("hello world");
+            num++;
+        }
+    }
+}
