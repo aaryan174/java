a865c28d2382a796ea027b78a0be7a05371d722e
aaryan174 <aryan007.lko@gmail.com>
2026-08-10 22:02:09 +0530

    Add loop program to print "hello world" 100 times

---
+++ loop.java
@@ -0,0 +1,9 @@
+public class loop {
+    public static void main(String[] args) {
+        int count = 0;
+        while (count < 100) {
+            System.out.println("hello world");
+            count++;
+        }
+    }
+}
