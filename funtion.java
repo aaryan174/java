b19331bd98887ff4068007b19983f0ba3f962da1
aaryan174 <aryan007.lko@gmail.com>
2026-08-13 15:22:14 +0530

    Add initial implementation of Hello World program with funtion

---
+++ funtion.java
@@ -0,0 +1,11 @@
+public class funtion {
+
+    public static void printHelloWorld(){
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+    }
+    public static void main(String[] args) {
+        printHelloWorld();
+    }
+}
